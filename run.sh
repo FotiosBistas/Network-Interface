a48313e1dff98e04b386319ff3cc47d5530793f9
FotiosBistas <fotis.bistas@gmail.com>
2024-03-23 10:54:40 +0200

    exit after failed compilation, capture pid of binary and exit if setcap did not succeed

--- run.sh
+++ run.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 cargo b --release 
 
+# Check if compilation succeeded
+ext=$?
+if [[ $ext -ne 0 ]]; then
+    echo "compilation failed with exit status $ext"
+    exit $ext
+fi
+
+# Set capabilities to the binary
 sudo setcap CAP_NET_ADMIN=eip target/release/tcp
 
-pid=$!
-target/release/tcp
-trap "kill $pid" INT TERM
-wait $pid
+# Check if setcap succeeded
+ext=$?
+if [[ $ext -ne 0 ]]; then
+    echo "setcap failed with exit status $ext"
+    exit $ext
+fi
 
+# Run the binary in the background
+target/release/tcp &
+# Capture the PID of the background process
+pid=$!  
+
+
+# Set up a trap to handle interrupt and terminate signals
+trap "echo 'Signal caught, killing process $pid'; kill $pid" INT TERM
+
+# Wait for the background process to finish
+wait $pid
